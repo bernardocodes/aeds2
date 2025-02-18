f884966ee0edc1ecea747ddeca3140d8f7652f99
Bernardo <bernardo.polaki@gmail.com>
2025-02-18 15:37:20 -0300

    Q2 - FINALIZADA

--- tps/TP01/q2_tp01/main.c
+++ tps/TP01/q2_tp01/main.c
@@ -2,24 +2,23 @@
 #include <string.h>
 
 void leStr(char *palavra){
-	getchar();
-	scanf("%s",palavra);
+	getchar();//para garantir que não haja lixo no teclado (leitura de multiplos num e char)
+	scanf("%[^\n]",palavra);
 }
 
 void inverte(char *s1, char *s2){
-	int i = 0,n=0;
-	int j = (strlen(s1) - 1);//pra não pegar a posição do \0
+	int i = 0;
+	int j = (strlen(s1) - 1);//setar para a posição da última letra (e não o \0)
 	do{
 		s2[i] = s1[j];
 		i++;
 		j--;	
-		n++;
-	}while(j>=0);
-	s2[i] = '\0';//evita \0 residual
+	}while(j>=0);// = (para incluir a primeira posição da str)
+	s2[i] = '\0';
 }
 
 void verificaIgual(char *s1, char *s2){	
-	if(strcmp(s1,s2)==0){//verifica se são iguais 
+	if(strcmp(s1,s2)==0){//verifica se são iguais (palíndromos)
 		printf("SIM\n");
 	}else{
 		printf("NAO\n");
@@ -27,49 +26,19 @@ void verificaIgual(char *s1, char *s2){
 }
 
 int main(){
-	int tam = 100;//mem
+	int tam = 200;
 	char s1[tam];
 	char s2[tam];
 	
-	leStr(s1);//leitura
+	leStr(s1);//leitura da primeira str
 	inverte(s1,s2);
 	verificaIgual(s1,s2);
 
-	while(strcmp(s1,"FIM")!=0){
+	while(strcmp(s1,"FIM")!=0){//leitura das demais
 		leStr(s1);
 		inverte(s1,s2);
 		verificaIgual(s1,s2);
 	}
 
-/*
-
-	inverte(s1,s2);
-
-	verificaIgual(s1,s2);
-
-
-	printf("\ns1 = %s",s1);//sai
-	printf("\ns2 = %s",s2);
-
-	puts("");a
-*/	
 	return 0;
 }
-
-/*
-	int tam = 10;//mem
-	char s1[tam];
-	char s2[tam];
-
-	scanf("%s",s1);//leitura
-	scanf("%s",s2);
-
-	if(strcmp(s1,s2)==0){//verifica se são iguais 
-		printf("IGUAIS\n");
-	}else{
-		printf("DIFERENTES\n");
-	}
-
-	printf("\ns1 = %s",s1);//sai
-	printf("\ns2 = %s",s2);
-*/
