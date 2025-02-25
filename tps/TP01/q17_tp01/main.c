1144d5f874bb4efefe6fcfd9b964a849fd80f471
Bernardo <bernardo.polaki@gmail.com>
2025-02-25 20:42:27 -0300

    PRONTO PRA LANÇAR
    Esse código foi reaproveitado da questão 2, e agr possue um método recursivo que faz o que se pede.

--- tps/TP01/q17_tp01/main.c
+++ tps/TP01/q17_tp01/main.c
@@ -1,34 +1,67 @@
-
 #include <stdio.h>
 #include <string.h>
 
-void leStr(char * str){//leitura
-	//getchar();
-	scanf("%[^\n]",str);
-//	printf("str = %s\n",str);	
+void leStr(char *palavra){
+	//getchar();//para garantir que não haja lixo no teclado
+	scanf("%[^\n]%*c",palavra);
 }
 
-void inverte(char *palavra, char *invertida){
-	int n = strlen(palavra)-1;
-//	printf("tam = %d\n",n);
-	int i;	
-	for(i=0;i<=n+1;i++){//no outro eu fiz com o do-while
-		invertida[i] = palavra[n];
-		n--;
+//na chamada, define sempre i=0 e j=(strlen(s1) - 1);
+void inverteRecursivo(char *s1, char *s2, int i, int j){	
+	if(j>=0){
+		s2[i] = s1[j];
+		i++;
+		j--;
+		inverteRecursivo(s1,s2,i,j);
+	}else{
+		s2[i] = '\0';
+		return;
 	}
-	invertida[i] = '\0';	
+}
+
+/*
+void inverte(char *s1, char *s2){
+	int i = 0;
+	int j = (strlen(s1) - 1);//setar para a posição da última letra (e não o \0)
+	do{
+		s2[i] = s1[j];
+		i++;
+		j--;	
+	}while(j>=0);// = (para incluir a primeira posição da str)
+	s2[i] = '\0';
+}
+*/
+
+void verificaIgual(char *s1, char *s2){//palíndromo???	
+	if(strcmp(s1,s2)==0){
+		printf("SIM\n");
+	}else{
+		printf("NAO\n");
+	}
 }
 
 int main(){
-	int tam = 10;//mudar tam depois
-	char palavra[tam];
-	char invertida[tam];
+	int tam = 2000;
+	char s1[tam];
+	char s2[tam];
+	
+	leStr(s1);//leitura da primeira str
 
-	leStr(palavra);	
-	printf("str = %s\n",palavra);
-	inverte(palavra,invertida);
+	if(strcmp(s1,"FIM")!=0){//evita análise da str "fim"
+		//inverte(s1,s2);
+		inverteRecursivo(s1,s2,0,(strlen(s1) - 1));//AQ
+		verificaIgual(s1,s2);
+	}
 
-	printf("chegou = %s\n",invertida);
+	while(strcmp(s1,"FIM")!=0){//leitura das demais
+		leStr(s1);
+		if(strcmp(s1,"FIM")!=0){//evita análise da str "fim" 
+			//inverte(s1,s2);	
+			inverteRecursivo(s1,s2,0,(strlen(s1) - 1));//AQ
+			verificaIgual(s1,s2);
+		}
+	}
+
 	return 0;
 }
 
