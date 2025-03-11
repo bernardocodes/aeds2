5c42a4d0cbf850ed06cc7b935f0cb8964779ed42
Bernardo <bernardo.polaki@gmail.com>
2025-03-10 21:09:31 -0300

    100%

--- tps/TP01/q1_tp01/Main.java
+++ tps/TP01/q1_tp01/Main.java
@@ -1,6 +1,27 @@
 
-public class Main{
+public class Main {
 	public static void main(String[] args){
-		System.out.println("hello, world!");
+
+		String s1;
+		s1 = MyIO.readLine();//primeira leitura
+			
+		if(s1.compareTo("FIM")!=0){
+			
+			do{
+				StringBuilder sb = new StringBuilder(s1);
+				sb.reverse();//inverte
+
+				String s2 = sb.toString();
+
+				if(s1.compareTo(s2)==0){//verifica pali
+					MyIO.println("SIM");
+				}else{
+					MyIO.println("NAO");
+				}	
+
+				s1 = MyIO.readLine();//nova leitura
+
+			}while(s1.compareTo("FIM")!=0);	
+		}
 	}
 }
